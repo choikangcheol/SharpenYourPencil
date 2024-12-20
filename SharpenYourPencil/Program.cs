b19e212cfc25cedf7ead2c589e20635e134bfae8
choikangcheol <skynlake@gmail.com>
2024-12-20 16:53:56 +0900

    Feat: 입력을 처리하는 무한루프와 종료하는 코드 추가

--- SharpenYourPencil/Program.cs
+++ SharpenYourPencil/Program.cs
@@ -13,7 +13,13 @@ namespace SharpenYourPencil
             Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
             Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
 
-
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                Console.WriteLine("Press Q to quit, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if (keyChar == 'Q' || keyChar == 'q') return;
+            }
         }
     }
 }
