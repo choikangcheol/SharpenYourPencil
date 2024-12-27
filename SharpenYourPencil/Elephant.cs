020cf4497c08bb1b08614526e83002425fa0a4d7
choikangcheol <skynlake@gmail.com>
2024-12-27 15:15:45 +0900

    Feat: Elephant 인스턴스에 메시지를 보내는 메서드 추가

--- SharpenYourPencil/Elephant.cs
+++ SharpenYourPencil/Elephant.cs
@@ -16,5 +16,11 @@ namespace SharpenYourPencil
             Console.WriteLine($"My name is {Name}.");
             Console.WriteLine($"My ears are {EarSize} inches tall.");
         }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine($"{Name} heard a message");
+            Console.WriteLine($"{whoSaidIt.Name} said this: {message}");
+        }
     }
 }
