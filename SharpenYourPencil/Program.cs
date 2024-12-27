d485bdf01a415ea31d528ee6e92235b80cc0803e
choikangcheol <skynlake@gmail.com>
2024-12-27 14:35:07 +0900

    Feat: 각 Elephant 인스턴스의 WhoAmI 메서드 호출 및 인스턴스를 서로 교환하는 기능 추가

--- SharpenYourPencil/Program.cs
+++ SharpenYourPencil/Program.cs
@@ -12,13 +12,39 @@ namespace SharpenYourPencil
         {
             Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
             Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
-
-            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+                        
             while (true)
             {
+                Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+                string line = Console.ReadLine();
+                int num;
+                if (!int.TryParse(line, out num)) continue;
+                Console.WriteLine($"You pressed {num}");
+
+                switch (num)
+                {
+                    case 1:
+                        Console.WriteLine("Calling lloyd.WhoAmI()");
+                        lloyd.WhoAmI();
+                        break;
+                    case 2:
+                        Console.WriteLine("Calling lucinda.WhoAmI()");
+                        lucinda.WhoAmI();
+                        break;
+                    case 3:
+                        Console.WriteLine("References have been swapped");
+                        Elephant temp = lucinda;
+                        lucinda = lloyd;
+                        lloyd = temp;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine();
+
                 Console.WriteLine("Press Q to quit, any other key to continue");
                 char keyChar = Console.ReadKey(true).KeyChar;
-                if (keyChar == 'Q' || keyChar == 'q') return;
+                if (keyChar == 'Q' || keyChar == 'q') break;
             }
         }
     }
